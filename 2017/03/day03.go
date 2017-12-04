e3ddce186a5bb1ca300d6f9b56f959f55d6e70ad
otibsa <otibsa@gmail.com>
2017-12-04 12:06:58 +0100

    2017 day03 part2

--- 2017/03/day03.go
+++ 2017/03/day03.go
@@ -29,15 +29,27 @@ func get_layer(n int) int {
 	return int(math.Floor(0.5*(math.Sqrt(float64(n)-1)+1)))
 }
 
-func walk_spiral(n int) int {
+func get_value(field map[position]int, x int, y int) int {
+	if v, ok := field[position{x,y}]; ok {
+		return v
+	} else {
+		return 0
+	}
+}
+
+func walk_spiral(n int) (int, int) {
 	var x, y, layer, layer_low int
 	var tp1, tp2, tp3, tp4 int
 	
+	// part 2
+	field2 := make(map[position]int)
+	field2[position{0,0}] = 1
+	first_larger := 0
+
 	if n == 1 {
-		return 0
+		return 0, 0
 	}
 	for i := 2; i <= n; i++ {
-		
 		if get_layer(i) > layer {
 			layer = get_layer(i)
 			layer_low = int(math.Pow(2*float64(layer)-1, 2) + 1)
@@ -51,30 +63,45 @@ func walk_spiral(n int) int {
 			//fmt.Printf("[%v]\tlayer_low=%v   tps: %v, %v, %v, %v\n", layer, layer_low, tp1, tp2, tp3, tp4)
 		}
 
+		sum := 0
 		switch {
 		case tp1 <= i && i < tp2:
 			// heading "north"
-			//fmt.Printf("heading north, ")
 			y++
+			sum += get_value(field2, x-1, y-1)
+			sum += get_value(field2, x-1, y)
+			sum += get_value(field2, x-1, y+1)
+			sum += get_value(field2, x, y-1)
 		case tp2 <= i && i < tp3:
 			// heading "west"
-			//fmt.Printf("heading west, ")
 			x--
+			sum += get_value(field2, x+1, y-1)
+			sum += get_value(field2, x, y-1)
+			sum += get_value(field2, x-1, y-1)
+			sum += get_value(field2, x+1, y)
 		case tp3 <= i && i < tp4:
 			// heading "south"
-			//fmt.Printf("heading south, ")
 			y--
+			sum += get_value(field2, x+1, y+1)
+			sum += get_value(field2, x+1, y)
+			sum += get_value(field2, x+1, y-1)
+			sum += get_value(field2, x, y+1)
 		case tp4 <= i || i == layer_low:
 			// heading "east"
-			//fmt.Printf("heading east, ")
 			x++
-		default:
-			fmt.Printf("??? i=%v tp4=%v layer_low=%v:  last case: %v\n", i, tp4, layer_low, (tp4 <= i || i == layer_low))
+			sum += get_value(field2, x-1, y+1)
+			sum += get_value(field2, x, y+1)
+			sum += get_value(field2, x+1, y+1)
+			sum += get_value(field2, x-1, y)
 		}
-		//fmt.Printf("i=% 3v   x=% 3v   y=% 3v\n", i, x, y)
+		field2[position{x,y}] = sum
+		if sum >= n && first_larger == 0{
+			first_larger = sum
+		}
+		fmt.Printf("i=% 3v   x=% 3v   y=% 3v   sum=% 5v\n", i, x, y, sum)
 	}
 
-	return hamming_distance(x, y)
+	return hamming_distance(x, y), first_larger
 }
 
 func build_field(n int) []position {
@@ -98,7 +125,9 @@ func process(r io.Reader) (string, string) {
 	n, err := strconv.Atoi(s)
 	check_err(err)
 
-	return strconv.Itoa(walk_spiral(n)), ""
+	part1, part2 := walk_spiral(n)
+
+	return strconv.Itoa(part1), strconv.Itoa(part2)
 	// var field []position
 	// field = build_field(n)
 	// pos := field[n]
@@ -112,10 +141,10 @@ func main() {
 	defer input.Close()
 
 	tests := []test_input{
-		{strings.NewReader("1"), "0", ""},
-		{strings.NewReader("12"), "3", ""},
+		//{strings.NewReader("1"), "0", ""},
+		//{strings.NewReader("12"), "3", ""},
 		{strings.NewReader("23"), "2", ""},
-		{strings.NewReader("1024"), "31", ""},
+		//{strings.NewReader("1024"), "31", ""},
 	}
 	for _, t := range tests {
 		sol_1, sol_2 := process(t.input)
