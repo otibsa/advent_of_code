5f34e9b97335bd2ddfa4e9f46257085c0971a3ea
otibsa <otibsa@gmail.com>
2017-12-04 15:39:03 +0100

    2017 day04 part1

---
+++ 2017/04/day04.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"bufio"
+	"strconv"
+)
+
+type test_input struct {
+	input    io.Reader
+	output_1 string
+	output_2 string
+}
+
+func process(r io.Reader) (string, string) {
+	scanner := bufio.NewScanner(r)
+
+	valid_counter := 0
+	var valid bool
+	// for each line:
+	for scanner.Scan() {
+		valid = true
+		// for each word:
+		words := strings.Fields(scanner.Text())
+		for i := 0; i < len(words)-1; i++ {
+			for j := i+1; j < len(words); j++ {
+				if words[i] == words[j] {
+					valid = false
+				}
+			}
+		}
+		if valid {
+			valid_counter++
+		}
+	}
+	return strconv.Itoa(valid_counter), ""
+}
+
+func main() {
+	input, err := os.Open("input.txt")
+	check_err(err)
+	defer input.Close()
+
+	tests := []test_input{
+		{strings.NewReader("aa bb cc dd ee"), "1", ""},
+		{strings.NewReader("aa bb cc dd aa"), "0", ""},
+		{strings.NewReader("aa bb cc dd aaa"), "1", ""},
+	}
+	for _, t := range tests {
+		sol_1, sol_2 := process(t.input)
+		if t.output_1 != "" && t.output_1 != sol_1 {
+			fmt.Printf("Part 1 test input \"%v\" failed: got \"%v\", wanted \"%v\"\n", t.input, sol_1, t.output_1)
+		}
+		if t.output_2 != "" && t.output_2 != sol_2 {
+			fmt.Printf("Part 2 test input \"%v\" failed: got \"%v\", wanted \"%v\"\n", t.input, sol_2, t.output_2)
+		}
+	}
+
+	part1, part2 := process(input)
+	fmt.Println("Solution for part 1: \"" + part1 + "\"")
+	fmt.Println("Solution for part 2: \"" + part2 + "\"")
+}
+
+func check_err(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
