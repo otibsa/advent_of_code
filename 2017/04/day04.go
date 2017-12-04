33edb57f6676ddc7cb35933cb8da1309d61f0fad
otibsa <otibsa@gmail.com>
2017-12-04 16:54:53 +0100

    2017 day04 part2

--- 2017/04/day04.go
+++ 2017/04/day04.go
@@ -15,14 +15,31 @@ type test_input struct {
 	output_2 string
 }
 
+func get_anagrams(word string) []string {
+	anagrams := []string{}
+	if len(word) == 1 {
+		return []string{word}
+	}
+	for i, c := range word {
+		rem := word[:i] + word[i+1:]
+		for _, anagram := range get_anagrams(rem) {
+			anagrams = append(anagrams, string(c)+anagram)
+		}
+	}
+	return anagrams
+}
+
 func process(r io.Reader) (string, string) {
 	scanner := bufio.NewScanner(r)
 
 	valid_counter := 0
-	var valid bool
+	valid_anagram_counter := 0
+	var valid, valid_anagram bool
+	_ = valid_anagram
 	// for each line:
 	for scanner.Scan() {
 		valid = true
+		valid_anagram = true
 		// for each word:
 		words := strings.Fields(scanner.Text())
 		for i := 0; i < len(words)-1; i++ {
@@ -30,13 +47,21 @@ func process(r io.Reader) (string, string) {
 				if words[i] == words[j] {
 					valid = false
 				}
+				for _, anagram := range get_anagrams(words[j]) {
+					if words[i] == anagram {
+						valid_anagram = false
+					}
+				}
 			}
 		}
 		if valid {
 			valid_counter++
 		}
+		if valid_anagram {
+			valid_anagram_counter++
+		}
 	}
-	return strconv.Itoa(valid_counter), ""
+	return strconv.Itoa(valid_counter), strconv.Itoa(valid_anagram_counter)
 }
 
 func main() {
@@ -48,6 +73,12 @@ func main() {
 		{strings.NewReader("aa bb cc dd ee"), "1", ""},
 		{strings.NewReader("aa bb cc dd aa"), "0", ""},
 		{strings.NewReader("aa bb cc dd aaa"), "1", ""},
+
+		{strings.NewReader("abcde fghij"), "", "1"},
+		{strings.NewReader("abcde xyz ecdab"), "", "0"},
+		{strings.NewReader("a ab abc abd abf abj"), "", "1"},
+		{strings.NewReader("iiii oiii ooii oooi oooo"), "", "1"},
+		{strings.NewReader("oiii ioii iioi iiio"), "", "0"},
 	}
 	for _, t := range tests {
 		sol_1, sol_2 := process(t.input)
