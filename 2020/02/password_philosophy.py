fd5a1d38b6bfcf2492e52aca57b82d13c84427e5
otibsa <otibsa@gmail.com>
2020-12-02 21:11:15 +0100

    2020 day02 part2
    Almost the same structure as part 1, only the check for a valid password
    slightly changes (line parsing stays the same)

--- 2020/02/password_philosophy.py
+++ 2020/02/password_philosophy.py
@@ -6,6 +6,7 @@ TEST_INPUT = """1-3 a: abcde
 2-9 c: ccccccccc"""
 
 TEST_OUTPUT = "2"
+TEST_OUTPUT_PART2 = "1"
 
 def get_lines(func):
     def wrapped(s):
@@ -14,9 +15,16 @@ def get_lines(func):
 
 @get_lines
 def part1(lines):
+    return count_valid_passwords(lines, is_valid_password)
+
+@get_lines
+def part2(lines):
+    return count_valid_passwords(lines, is_valid_password2)
+
+def count_valid_passwords(lines, cond):
     i = 0
     for line in lines:
-        if is_valid_password(*parse_line(line)):
+        if cond(*parse_line(line)):
             i += 1
     return i
 
@@ -27,9 +35,21 @@ def parse_line(line):
 def is_valid_password(_min, _max, letter, password):
     return _min <= len([c for c in password if c == letter]) <= _max
 
+def is_valid_password2(posA, posB, letter, password):
+    if password[posA-1] == letter:
+        return password[posB-1] != letter
+    if password[posB-1] == letter:
+        return password[posA-1] != letter
+    return False
+
 def test_part1():
     assert part1(TEST_INPUT) == TEST_OUTPUT
 
+def test_part2():
+    assert part2(TEST_INPUT) == TEST_OUTPUT_PART2
+
 if __name__ == "__main__":
     with open("input.txt") as f:
-        print(part1(f.read()))
+        input = f.read()
+        print(part1(input))
+        print(part2(input))
