c4437861eef25106fc213e9b056306d7eb61d9d3
otibsa <otibsa@gmail.com>
2020-12-02 19:39:45 +0100

    2020 day01 part2
    Reuse the code from part1 by making the sum a variable instead of a
    constant 2020.

--- 2020/01/report_repair.py
+++ 2020/01/report_repair.py
@@ -4,7 +4,8 @@ TEST_INPUT = """1721
 299
 675
 1456"""
-TEST_OUTPUT = "514579"
+TEST_OUTPUT_PART1 = "514579"
+TEST_OUTPUT_PART2 = "241861950"
 
 def string_int_list(f):
     def wrapped(s):
@@ -13,17 +14,37 @@ def string_int_list(f):
 
 @string_int_list
 def part1(xs):
+    return _part1(xs)
+
+@string_int_list
+def part2(xs):
+    return _part2(xs)
+
+def _part1(xs, summed=2020):
     for i in range(len(xs)):
-        if 2020-xs[i] in xs[i:]:
-            return xs[i]*(2020-xs[i])
+        if summed-xs[i] in xs[i:]:
+            return xs[i]*(summed-xs[i])
     raise Exception("no solution found")
 
+def _part2(xs):
+    for i in range(len(xs)):
+        try:
+            prod2 = _part1(xs[i:], summed=2020-xs[i])
+            return xs[i] * prod2
+        except:
+            pass
+
 def test_part1():
-    assert part1(TEST_INPUT) == TEST_OUTPUT
+    assert part1(TEST_INPUT) == TEST_OUTPUT_PART1
+
+def test_part2():
+    assert part2(TEST_INPUT) == TEST_OUTPUT_PART2
 
 def main():
     with open('input.txt') as f:
-        print(part1(f.read()))
+        input = f.read()
+        print(part1(input))
+        print(part2(input))
 
 if __name__ == "__main__":
     main()
