f6e49af59f7b8275dc26de376b9f2cb35d14ad10
otibsa <otibsa@gmail.com>
2020-12-03 20:49:03 +0100

    2020 day03 part2
    The additional parameter was either going to be the starting position or
    the slope. Part 2 iterates over multiple slopes, reusing the existing
    code with the slope as a parameter. The slope with two steps down
    required a modulo check.

--- 2020/03/toboggan_trajectory.py
+++ 2020/03/toboggan_trajectory.py
@@ -13,6 +13,7 @@ TEST_INPUT = """..##.......
 .#..#...#.#"""
 
 TEST_OUTPUT = "7"
+TEST_OUTPUT_PART2 = "336"
 
 def get_lines(func):
     def wrapped(s):
@@ -23,11 +24,19 @@ def get_lines(func):
 def part1(lines):
     return count_trees(lines)
 
-def count_trees(lines):
+@get_lines
+def part2(lines):
+    trees = 1
+    for slope in [(1,1), (3,1), (5,1), (7,1), (1,2)]:
+        trees *= count_trees(lines, step=slope[0], stepY=slope[1])
+    return trees
+
+def count_trees(lines, step=3, stepY=1):
     pos=0
-    step=3
     trees = 0
-    for line in lines:
+    for y, line in enumerate(lines):
+        if y%stepY != 0:
+            continue
         if line[pos%len(line)] == "#":
             trees += 1
         pos += step
@@ -36,6 +45,11 @@ def count_trees(lines):
 def test_part1():
     assert part1(TEST_INPUT) == TEST_OUTPUT
 
+def test_part2():
+    assert part2(TEST_INPUT) == TEST_OUTPUT_PART2
+
 if __name__ == "__main__":
     with open(os.path.dirname(os.path.realpath(__file__))+"/input.txt") as f:
-        print(part1(f.read()))
+        input = f.read()
+        print(part1(input))
+        print(part2(input))
