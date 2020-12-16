e48d3f7d04ae60bd11f8da828d78832d8c68b90e
otibsa <otibsa@gmail.com>
2020-12-16 13:32:14 +0100

    2020 day05 part1
    The task is to find the highest seat ID, where the seat ID is encoded as
    a binary number with L/F instead of 0 and B/R instead of 1.

---
+++ 2020/05/binary_boarding.py
@@ -0,0 +1,47 @@
+import os
+
+EXAMPLES = {
+    "part1": {
+        "BFFFBBFRRR": 567,
+        "FFFBBBFRRR": 119,
+        "BBFFBBFRLL": 820,
+        """BFFFBBFRRR
+        FFFBBBFRRR
+        BBFFBBFRLL""": 820
+    }
+}
+
+def get_lines(func):
+    def wrapped(s):
+        return str(func([line for line in s.split("\n") if line != ""]))
+    return wrapped
+
+@get_lines
+def part1(lines):
+    return max(decode_seat(bsp) for bsp in lines)
+
+def decode_seat(bsp):
+    """
+    The Binary Seat Partition is a binary number with 'F'/'L' instead of '0' and
+    'B'/'R' instead of '1'.
+    """
+    seat_id = 0
+    for c in bsp:
+        seat_id <<= 1
+        if c in ["F", "L"]:
+            pass
+        elif c in ["B", "R"]:
+            seat_id += 1
+    return seat_id
+    # alternative with O(4*n):
+    # return int(bsp.replace("F","0").replace("L","0").replace("B","1").replace("R","1"), 2)
+
+def test_examples():
+    for func_name, example_dict in EXAMPLES.items():
+        for input, output in example_dict.items():
+            assert globals()[func_name](input) == str(output)
+
+if __name__ == "__main__":
+    with open(os.path.dirname(os.path.realpath(__file__))+"/input.txt") as f:
+        input = f.read()
+        print(part1(input))
