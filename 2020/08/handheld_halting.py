1faf92364354ee37715ffd42d2f38e1935ffec79
otibsa <otibsa@gmail.com>
2020-12-17 11:53:56 +0100

    2020 day08 part2

--- 2020/08/handheld_halting.py
+++ 2020/08/handheld_halting.py
@@ -1,6 +1,7 @@
 import os
 import textwrap
 import re
+import copy
 
 EXAMPLES = {
     "part1": {
@@ -16,6 +17,19 @@ EXAMPLES = {
         acc +6
         """: 5
     },
+    "part2": {
+        """\
+        nop +0
+        acc +1
+        jmp +4
+        acc +3
+        jmp -3
+        acc -99
+        acc +1
+        jmp -4
+        acc +6
+        """: 8
+    }
 }
 
 def get_lines(func):
@@ -28,11 +42,29 @@ def part1(lines):
     state = {
         "pc": 0,
         "past": [],
-        "program": lines,
         "acc": 0,
         "halted": False
     }
-    state = run(state)
+    state = run(state, program=lines)
+    return state["acc"]
+
+@get_lines
+def part2(lines):
+    """
+    When we do a swap, clear the swap flag on the swapped path, and keep track
+    of the next state of the unswapped path. There is only ever one swap, so we
+    don't need multiple backtrack states and the swap flag does not need to be a
+    counter.
+    """
+    state = {
+        "pc": 0,
+        "past": [],
+        "acc": 0,
+        "halted": False,
+        "swap": True,
+        "backtrack_state": {}
+    }
+    state = run(state, program=lines)
     return state["acc"]
 
 def cmd_nop(arg, state):
@@ -48,21 +80,30 @@ def cmd_jmp(arg, state):
     state["pc"] += arg
     return state
 
-def run(state, debug=False):
+def run(state, program, debug=False):
     if debug:
-        for i, line in enumerate(state["program"]):
+        for i, line in enumerate(program):
             print(f"{i: 4}: {line}")
     while not state["halted"]:
         if state["pc"] < 0:
             state["halted"] = True
             state["error"] = "Negative pc"
             continue
-        if state["pc"] in state["past"]:
+        if state["pc"] >= len(program):
+            # finished successfully
             state["halted"] = True
+            continue
+        if state["pc"] in state["past"]:
+            # we ran into an infinite loop
+            if state.get("backtrack_state"):
+                # backtrack to where we did the swap (there is only ever one swap)
+                state = state["backtrack_state"]
+            else:
+                state["halted"] = True
             continue
 
         # fetch
-        instruction = state["program"][state["pc"]]
+        instruction = program[state["pc"]]
 
         # decode
         cmd = lambda a, s: s.update({"halted": True, "error": f"Unknown opcode"})
@@ -70,6 +111,16 @@ def run(state, debug=False):
         if m := re.match(r"(.{3}) ([+-][0-9]+)", instruction):
             opcode = m.group(1)
             arg = int(m.group(2))
+            if state.get("swap") and opcode in ["nop", "jmp"]:
+                bs = copy.deepcopy(state)
+                # remember the *next* state (non-swapped), otherwise we're in a loop
+                if opcode == "nop":
+                    state["backtrack_state"] = cmd_nop(arg, bs)
+                    opcode = "jmp"
+                elif opcode == "jmp":
+                    state["backtrack_state"] = cmd_jmp(arg, bs)
+                    opcode = "nop"
+                state["swap"] = False
             if opcode == "nop":
                 cmd = cmd_nop
             elif opcode == "acc":
@@ -91,7 +142,8 @@ def run(state, debug=False):
         for i in range(state["pc"]-5, state["pc"]+5):
             if i < 0:
                 continue
-            print(f"{i: 5}: {state['program'][i]}")
+            print(f"{i: 5}: {program[i]}")
+
     return state
 
 def test_examples():
@@ -103,3 +155,4 @@ if __name__ == "__main__":
     with open(os.path.dirname(os.path.realpath(__file__))+"/input.txt") as f:
         input = f.read()
         print(part1(input))
+        print(part2(input))
