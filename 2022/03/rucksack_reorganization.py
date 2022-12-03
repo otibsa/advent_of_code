7a447cb2cb3c81679145142080e5b23b989ba787
otibsa <otibsa@gmail.com>
2022-12-03 12:30:59 +0100

    2022 day03 part2

--- 2022/03/rucksack_reorganization.py
+++ 2022/03/rucksack_reorganization.py
@@ -10,6 +10,15 @@ wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw""": 157
     },
+    "part2": {
+        """\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw""": 70
+    }
 }
 
 def line_generator(filename):
@@ -17,17 +26,26 @@ def line_generator(filename):
         while line := f.readline():
             yield line.strip("\n")
 
+def rucksack_generator(line_gen):
+    for r in line_gen:
+        yield (r[:len(r)//2], r[len(r)//2:])
+
+def group_generator(line_gen):
+    for r1 in line_gen:
+        r2=next(line_gen)
+        r3=next(line_gen)
+        yield (r1, r2, r3)
+
 def part1(lines):
-    return find_common(lines)
+    return find_common(rucksack_generator(lines))
 
 def part2(lines):
-    pass
+    return find_common(group_generator(lines))
 
-def find_common(rucksacks):
+def find_common(tuple_gen):
     sum_prio = 0
-    for rucksack in rucksacks:
-        left, right = rucksack[:len(rucksack)//2], rucksack[len(rucksack)//2:]
-        common_items = set(left)&set(right)
+    for compartments in tuple_gen:
+        common_items = set.intersection(*map(set, compartments))
         for item in common_items:
             offset = ord("A")-27 if "A" <= item <= "Z" else ord("a")-1
             sum_prio += ord(item)-offset
@@ -37,7 +55,8 @@ if __name__ == "__main__":
     filename = os.path.dirname(os.path.realpath(__file__))+"/input.txt"
     for part, example_dict in EXAMPLES.items():
         for input, expected_output in example_dict.items():
-            output = globals()[part](input.split("\n"))
+            input = (line for line in input.split("\n"))
+            output = globals()[part](input)
             assert output == expected_output, f"{part}(...)={output}, should have been {expected_output}"
     print(part1(line_generator(filename)))
     print(part2(line_generator(filename)))
