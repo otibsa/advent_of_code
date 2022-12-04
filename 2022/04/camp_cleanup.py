0506fdf82fb933e8e536832183f74aed40cb519a
otibsa <otibsa@gmail.com>
2022-12-04 14:41:13 +0100

    2022 day04 part1

---
+++ 2022/04/camp_cleanup.py
@@ -0,0 +1,44 @@
+import os
+import re
+
+EXAMPLES = {
+    "part1": {
+        """\
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8""": 2
+    },
+}
+
+def line_generator(filename):
+    with open(filename) as f:
+        while line := f.readline():
+            yield line.strip("\n")
+
+def part1(lines):
+    return find_subsets(lines)
+
+def part2(lines):
+    pass
+
+def find_subsets(lines):
+    nr_subsets = 0
+    for assignment in lines:
+        if m := re.match(r"(.*)-(.*),(.*)-(.*)", assignment):
+            ll,lh,rl,rh = map(int, m.groups())
+            if (ll<=rl<=lh and ll<=rh<=lh) or (rl<=ll<=rh and rl<=lh<=rh):
+                nr_subsets += 1
+    return nr_subsets
+
+if __name__ == "__main__":
+    filename = os.path.dirname(os.path.realpath(__file__))+"/input.txt"
+    for part, example_dict in EXAMPLES.items():
+        for input, expected_output in example_dict.items():
+            input = (line for line in input.split("\n"))
+            output = globals()[part](input)
+            assert output == expected_output, f"{part}(...)={output}, should have been {expected_output}"
+    print(part1(line_generator(filename)))
+    print(part2(line_generator(filename)))
