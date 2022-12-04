467d2cb0cf14143f1cba257ed38fb0257133532c
otibsa <otibsa@gmail.com>
2022-12-04 15:03:51 +0100

    2022 day04 part2

--- 2022/04/camp_cleanup.py
+++ 2022/04/camp_cleanup.py
@@ -11,6 +11,15 @@ EXAMPLES = {
 6-6,4-6
 2-6,4-8""": 2
     },
+    "part2": {
+        """\
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8""": 4
+    }
 }
 
 def line_generator(filename):
@@ -19,19 +28,23 @@ def line_generator(filename):
             yield line.strip("\n")
 
 def part1(lines):
-    return find_subsets(lines)
+    return find_subsets(lines)[0]
 
 def part2(lines):
-    pass
+    return find_subsets(lines)[1]
 
 def find_subsets(lines):
     nr_subsets = 0
+    nr_overlaps = 0
     for assignment in lines:
         if m := re.match(r"(.*)-(.*),(.*)-(.*)", assignment):
             ll,lh,rl,rh = map(int, m.groups())
             if (ll<=rl<=lh and ll<=rh<=lh) or (rl<=ll<=rh and rl<=lh<=rh):
                 nr_subsets += 1
-    return nr_subsets
+            if ll<=rl<=lh or ll<=rh<=lh or rl<=ll<=rh or rl<=lh<=rh:
+                nr_overlaps += 1
+
+    return nr_subsets, nr_overlaps
 
 if __name__ == "__main__":
     filename = os.path.dirname(os.path.realpath(__file__))+"/input.txt"
