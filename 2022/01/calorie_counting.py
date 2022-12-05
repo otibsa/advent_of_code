dce991f8bd937fdb7e436ab03b7b86d90b46636d
otibsa <otibsa@gmail.com>
2022-12-05 12:10:17 +0100

    2022 day01 part2

--- 2022/01/calorie_counting.py
+++ 2022/01/calorie_counting.py
@@ -18,6 +18,24 @@ EXAMPLES = {
 
 10000
 """: 24000
+    },
+    "part2": {
+        """\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+""": 45000
     }
 }
 
@@ -26,7 +44,7 @@ def line_generator(filename):
         while line := f.readline():
             yield line.strip("\n")
 
-def part1(lines):
+def _part1(lines):
     max_calories = 0
     elf_sum = 0
     for line in lines:
@@ -38,8 +56,39 @@ def part1(lines):
             elf_sum += int(line)
     return max_calories
 
+def elf_generator(lines):
+    elf = []
+    for line in lines:
+        if line == "":
+            yield elf
+            elf = []
+        else:
+            elf += [line]
+
+
+def heavy_carriers(lines):
+    calories = [sum(int(line) for line in elf) for elf in elf_generator(lines)]
+    # calories = []
+    # for elf in elf_generator(lines):
+    #     elf_sum = 0
+    #     for line in elf:
+    #         elf_sum += int(line)
+    #     calories += [elf_sum]
+    return sorted(calories, reverse=True)
+
+def part1(lines):
+    return heavy_carriers(lines)[0]
+
+def part2(lines):
+    return sum(heavy_carriers(lines)[:3])
+
 if __name__ == "__main__":
     filename = os.path.dirname(os.path.realpath(__file__))+"/input.txt"
-    for input, output in EXAMPLES["part1"].items():
-        assert part1(input.split("\n")) == output, f"got {part1(input)}, should have been {output}"
+    for part, example_dict in EXAMPLES.items():
+        for input, expected_output in example_dict.items():
+            #assert part1(input.split("\n")) == output, f"got {_part1(input)}, should have been {output}"
+            print(f"{input=} {expected_output=}")
+            output = globals()[part](input.split("\n"))
+            assert output == expected_output, f"{part}(...)={output}, should have been {expected_output}"
     print(part1(line_generator(filename)))
+    print(part2(line_generator(filename)))
