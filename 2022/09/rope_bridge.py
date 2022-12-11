887a62761a3299ea3b536cac6a1e50a4b039ef49
otibsa <otibsa@gmail.com>
2022-12-11 20:04:10 +0100

    2022 day09 part2

--- 2022/09/rope_bridge.py
+++ 2022/09/rope_bridge.py
@@ -13,7 +13,28 @@ R 4
 D 1
 L 5
 R 2
-""": 13
+""": 13},
+    "part2": {
+        """\
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
+""": 1,
+        """\
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+""": 36,
     },
 }
 
@@ -27,34 +48,36 @@ def part1(lines):
     return sum(sum(visited[y].values()) for y in visited.keys())
 
 def part2(lines):
-    pass
+    visited = move_rope(lines, rope_len=10)
+    return sum(sum(visited[y].values()) for y in visited.keys())
 
-def get_size(visited, minimum=5):
-    min_y, max_y = min(0,0, *visited.keys()), max(5,5, *visited.keys())
-    min_x, max_x = 0, 5
+def print_grid(h, ts, visited, print_rope=True, print_visited=False):
+    min_y, max_y = min(0, h[0], *visited.keys()), max(5, h[0], *visited.keys())
+    min_x, max_x = min(0, h[1]), max(5, h[1])
     for y in range(min_y, max_y+1):
         min_x = min(min_x, min_x, *visited[y].keys())
         max_x = max(max_x, max_x, *visited[y].keys())
-    return range(min_y, max_y+1), range(min_x, max_x+1)
 
-def print_grid(h, t, visited, print_ends=True):
-    range_y, range_x = get_size(visited, 5)
+    range_y, range_x = range(min_y, max_y+1), range(min_x, max_x+1)
 
     for y in reversed(range_y):
         for x in range_x:
-            if y == h[0] and x == h[1] and print_ends:
-                c = "H"
-            elif y == t[0] and x == t[1] and print_ends:
-                c = "T"
-            elif visited[y][x]:
+            c = "."
+            if x == 0 and y == 0:
+                c = "s"
+            if print_visited and visited[y][x]:
                 c = "#"
-            else:
-                c = "."
+            for i,t in reversed(list(enumerate(ts))):
+                if y == t[0] and x == t[1] and print_rope:
+                    c = str(i+1)
+            if y == h[0] and x == h[1] and print_rope:
+                c = "H"
+
             print(c, end="")
         print()
     print()
 
-def move_rope(lines):
+def move_rope(lines, rope_len=2):
     visited = defaultdict(lambda: defaultdict(bool))
     deltas={
         "U": (1, 0),
@@ -62,8 +85,7 @@ def move_rope(lines):
         "R": (0, 1),
         "L": (0, -1),
     }
-    h = [0, 0]
-    t = [0, 0]
+    rope = [[0,0] for _ in range(rope_len)]
     for line in lines:
         m = re.match(r"([UDRL]) ([0-9]+)", line)
         if not m:
@@ -71,15 +93,20 @@ def move_rope(lines):
         direction = m.group(1)
         distance = int(m.group(2))
         # go baby steps
-        for _ in range(distance):
-            h = [h[yx] + deltas[direction][yx] for yx in [0,1]]
-            for yx in [0,1]:
-                d = h[yx] - t[yx]
-                if abs(d) > 1:
-                    t[1-yx] = h[1-yx]    # make sure we're not diagonal anymore
-                    t[yx] += d//abs(d)   # -1 or +1
+        for step in range(distance):
+            rope[0] = [rope[0][yx] + deltas[direction][yx] for yx in [0,1]]
+            for i in range(1, len(rope)):
+                d = [rope[i-1][yx] - rope[i][yx] for yx in [0,1]]
+                for yx in [0,1]:
+                    if abs(d[yx]) > 1:
+                        if not abs(d[1-yx]) > 1:
+                            # only move to the same row/column if it doesn't
+                            # take two steps (special case for part 2)
+                            rope[i][1-yx] = rope[i-1][1-yx]
+                        rope[i][yx] += d[yx]//abs(d[yx])
 
-            visited[t[0]][t[1]] = True
+            visited[rope[-1][0]][rope[-1][1]] = True
+        #print_grid(rope[0], rope[1:], visited)
 
     return visited
 
