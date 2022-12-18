f03f7f1d983500f149db92c86b95e8dc2e51245e
otibsa <otibsa@gmail.com>
2022-12-18 17:28:14 +0100

    2022 day18 part2

--- 2022/18/boiling_boulders.py
+++ 2022/18/boiling_boulders.py
@@ -1,6 +1,7 @@
 import os
 import re
 from collections import defaultdict
+from math import inf
 
 EXAMPLES = {
     "part1": {
@@ -20,6 +21,23 @@ EXAMPLES = {
 2,3,5
 """: 64,
     },
+    "part2": {
+        """\
+2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5
+""": 58,
+    },
 }
 
 def line_generator(filename):
@@ -28,23 +46,73 @@ def line_generator(filename):
             yield line.strip("\n")
 
 def part1(lines):
-    return calc_surface_area(lines)
+    _, area, _, _, _ = parse_input(lines)
+    return area
 
 def part2(lines):
-    pass
+    # run BFS starting outside a cube and mark all reached Air cubes with "+"
+    # for steam. This should leave air pockets untouched, then we can change all
+    # remaining air cubes to lava cubes, add them to the input list and run the
+    # area calculation again
+    lines = list(lines)
+    cubes, _, range_x, range_y, range_z = parse_input(lines)
+    cubes = flood(cubes, range_x, range_y, range_z)
+    lines = [",".join(map(str, c)) for c in cubes.keys() if cubes[c] in ".#"]
+    _, area, _, _, _ = parse_input(lines)
+    return area
 
-def calc_surface_area(lines):
-    cubes = defaultdict(bool)
+def flood(cubes, range_x, range_y, range_z):
+    deltas = [(-1,0,0), (1,0,0), (0,-1,0), (0,1,0), (0,0,-1), (0,0,1)]
+    min_x, max_x = min(range_x)-1, max(range_x)+1
+    min_y, max_y = min(range_y)-1, max(range_y)+1
+    min_z, max_z = min(range_z)-1, max(range_z)+1
+    queue = [(min_x, min_y, min_z)]
+    while queue:
+        v = queue[0]
+        queue = queue[1:]
+        for n in [vector_add(v, d) for d in deltas]:
+            nx,ny,nz = n
+            if nx<min_x or nx>max_x or ny<min_y or ny>max_y or nz<min_z or nz>max_z:
+                continue
+            if cubes[n] == ".":
+                cubes[n] = "+"
+                queue += [n]
+    return cubes
+
+def draw_cubes(cubes, range_x, range_y, range_z):
+    for z in range_z:
+        print(f"{z=}")
+        for y in range_y:
+            for x in range_x:
+                c = cubes[(x,y,z)]
+                #c = {"A": ".", "L": "#", "S": "+"}[c]
+                print(c, end="")
+            print()
+        print()
+
+def parse_input(lines):
+    cubes = defaultdict(lambda: ".")
     area = 0
+    deltas = [(-1,0,0), (1,0,0), (0,-1,0), (0,1,0), (0,0,-1), (0,0,1)]
+    min_x, max_x, min_y, max_y, min_z, max_z = inf, -inf, inf, -inf, inf, -inf
     for line in lines:
         if line == "":
             continue
         cube = tuple(map(int, line.split(",")))
-        deltas = [(-1,0,0), (1,0,0), (0,-1,0), (0,1,0), (0,0,-1), (0,0,1)]
-        touching = sum(cubes[n] for n in [vector_add(cube, d) for d in deltas])
+        x,y,z = cube
+        min_x, min_y, min_z = min(min_x, x), min(min_y, y), min(min_z, z)
+        max_x, max_y, max_z = max(max_x, x), max(max_y, y), max(max_z, z)
+        touching = sum(cubes[n]!="." for n in [vector_add(cube, d) for d in deltas])
         area += 6-2*touching
-        cubes[cube] = True
-    return area
+        cubes[cube] = "#"
+    range_x = range(min_x, max_x+1)
+    range_y = range(min_y, max_y+1)
+    range_z = range(min_z, max_z+1)
+
+    # ? Apparently the cube coordinates want to be initialized for the flooding
+    # to work
+    _ = [cubes[(x,y,z)] for z in range_z for y in range_y for x in range_x]
+    return cubes, area, range_x, range_y, range_z
 
 def vector_add(v1, v2):
     return tuple(map(sum, zip(v1,v2)))
