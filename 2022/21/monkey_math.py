07c91f02be1a4cd0feda4e3048d4f155f9d9d821
otibsa <otibsa@gmail.com>
2022-12-22 08:33:09 +0100

    2022 day21 part2

--- 2022/21/monkey_math.py
+++ 2022/21/monkey_math.py
@@ -3,6 +3,7 @@ import re
 from collections import defaultdict
 from math import inf
 from pprint import pprint
+from time import sleep
 
 EXAMPLES = {
     "part1": {
@@ -23,6 +24,25 @@ lgvd: ljgn * ptdq
 drzm: hmdt - zczc
 hmdt: 32
 """: 152,
+    },
+    "part2": {
+        """\
+root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32
+""": 301
     }
 }
 
@@ -37,24 +57,58 @@ def part1(lines):
     return int(root_number)
 
 def part2(lines):
-    pass
+    monkeys = parse_input(lines)
+    left_m, _, right_m = monkeys["root"]
+    monkeys["humn"] = "x"
+    left = dfs(monkeys, left_m)
+    right = dfs(monkeys, right_m)
+    if type(left) in [int, float]:
+        to_solve, solution = right, left
+    else:
+        to_solve, solution = left, right
+    s = solve(to_solve, solution)
+    return s
+
+def solve(to_solve, solution):
+    if to_solve == "x":
+        return solution
+    left, op, right = to_solve
+    if type(right) == int:
+        if op == "+":
+            return solve(left, solution - right)
+        elif op == "-":
+            return solve(left, solution + right)
+        elif op == "*":
+            return solve(left, solution // right)
+        elif op == "/":
+            return solve(left, solution * right)
+    elif type(left) == int:
+        #   20 / (X - 2) == 2
+        if op == "+":
+            return solve(right, solution - left)
+        elif op == "-":
+            return solve(right, left - solution)
+        elif op == "*":
+            return solve(right, solution // left)
+        elif op == "/":
+            return solve(right, left // solution)
 
 def dfs(monkeys, m):
-    if type(monkeys[m]) == int:
+    if type(monkeys[m]) in [int,float] or monkeys[m] == "x":
         return monkeys[m]
-    left = monkeys[m][0]
-    right = monkeys[m][2]
+    left, op, right = monkeys[m]
     left = dfs(monkeys, left)
     right = dfs(monkeys, right)
-    op = monkeys[m][1]
-    if op == "+":
-        return left + right
-    elif op == "-":
-        return left - right
-    elif op == "*":
-        return left * right
-    elif op == "/":
-        return left / right
+    if type(left) in [int,float] and type(right) in [int,float]:
+        if op == "+":
+            return left + right
+        elif op == "-":
+            return left - right
+        elif op == "*":
+            return left * right
+        elif op == "/":
+            return left // right
+    return (left, op, right)
 
 def parse_input(lines):
     monkeys = {}
